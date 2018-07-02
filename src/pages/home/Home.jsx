04a47f4d7ece1fbcbf069e70355a562b64db404c
Peter Binder <peterb@quince.hu>
2018-07-02 22:35:46 +0200

    Added home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class Home extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 Home
             </div>
